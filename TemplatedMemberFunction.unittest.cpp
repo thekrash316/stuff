c16043e90012faa5858ef71218871c9a426cfbb1
thekrash316 <thekrash316@kjdfiuer>
2015-12-14 17:11:45 +0000

    templated member function specializations

--- TemplatedMemberFunction.unittest.cpp
+++ TemplatedMemberFunction.unittest.cpp
@@ -58,3 +58,73 @@ TEST(MyClass, Exercise)
 
 	mc.ProcessMessage();
 }
+
+
+// testing template member specializations
+template <typename T>
+class Numbers
+{
+public:
+
+    template<typename U>
+    U get()
+    {
+        return U(0);
+    }
+};
+
+
+template<> template<>
+int Numbers<int>::get<int>()
+{
+    return 1;
+}
+
+
+template<> template<>
+float Numbers<int>::get<float>()
+{
+    return 2.0;
+}
+
+
+template<> template<>
+int Numbers<float>::get<int>()
+{
+    return 3;
+}
+
+
+template<> template<>
+float Numbers<float>::get<float>()
+{
+    return 4.0;
+}
+
+
+TEST(Numbers, ExerciseInt)
+{
+	Numbers<int> n;
+
+	ASSERT_EQ(1, n.get<int>());
+	ASSERT_EQ(2.0, n.get<float>());
+}
+
+
+TEST(Numbers, ExerciseFloat)
+{
+	Numbers<float> n;
+
+	ASSERT_EQ(3, n.get<int>());
+	ASSERT_EQ(4.0, n.get<float>());
+}
+
+
+TEST(Numbers, ExerciseAny)
+{
+	Numbers<short> n;
+
+	ASSERT_EQ(0, n.get<int>());
+	ASSERT_EQ(0.0, n.get<float>());
+}
+
